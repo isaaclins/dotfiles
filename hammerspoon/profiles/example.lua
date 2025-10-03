627b262f0ad94b89cc9521e119583bd9811101ea
isaaclins <isaac.lins07@gmail.com>
2025-10-03 22:07:13 +0200

    Add example and window management profiles for Hammerspoon with metadata and alert functionality.

---
+++ hammerspoon/profiles/example.lua
@@ -0,0 +1,5 @@
+-- ICON: 🚧 
+-- NAME: Example Profile 
+-- DESCRIPTION: This is an example description 
+-- IMPORTANT NOTE: ^ hs.chooser will always take the last one in the file. TODO: make it so that it takes the first one in the file?
+hs.alert.show("fun test!")
---
+++ hammerspoon/profiles/window-management.lua
@@ -0,0 +1,7 @@
+-- ICON: 1:
+-- NAME: Window Manager
+-- DESCRIPTION: Advanced window tiling and management
+-- AUTHOR: isaaclins
+-- AUTHOR_URL: https://github.com/isaaclins
+
+hs.alert.show("window management!")
